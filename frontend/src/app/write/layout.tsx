45bbe933484298b70349b6af1fddc70ded2c17fc
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-03-23 15:25:59 +0300

    removed unused quill dependancy <Quil />

--- frontend/src/app/write/layout.tsx
+++ frontend/src/app/write/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { twMerge } from "tailwind-merge";
-import 'react-quill/dist/quill.snow.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +19,6 @@ export default function RootLayout({
       <div
         className={twMerge(inter.className, "bg-white text-black antialiased")}
       >
-        <QuillStyles />
         {children}
       </div>
   );
